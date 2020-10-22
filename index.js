714ffa951e3e9d5ab35630e9320a44912f0b25be
Ki.BongJo <Kibongjo1017@gmail.com>
2020-10-22 14:58:56 +0900

    babel, nodemon 설치

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require("express");
+//node_modules에서 라이브러리 import (ES6)
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-  console.log(`http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
-function handleHome(req, res) {
-  res.send("Hello from home");
-}
+//Home으로 route
+const handleHome = (req, res) => res.send("Hello from home");
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+//Profile로 route
+const handleProfile = (req, res) => res.send("You are on my profile");
+
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
-
+//4000번 포트 열기
 app.listen(PORT, handleListening);
