41b7b1bfe352a10ed9202bb819f1c27775a1306d
Ki.BongJo <Kibongjo1017@gmail.com>
2020-10-27 23:54:23 +0900

    MVC -1

---
+++ routers/userRouter.js
@@ -0,0 +1,6 @@
+import express, { Router } from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+
---
+++ routers/videoRouter.js
@@ -0,0 +1,6 @@
+import express, { Router } from "express";
+
+const videoRouter = express.Router();
+
+export default videoRouter;
+
